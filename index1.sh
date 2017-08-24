3a9c726fe218996e472284f81af18670f3bde881
halfenif <halfenif@gmail.com>
2017-08-24 14:36:25 +0900

    Index Work.08.24 14:36

--- index1.sh
+++ index1.sh
@@ -1,16 +1,15 @@
-nohup python3 batch_index.py -p 7117  -s 0 -t board -y 2017 > nohup.7117 &
-nohup python3 batch_index.py -p 7116  -s 0 -t board -y 2016 > nohup.7116 &
-nohup python3 batch_index.py -p 7115  -s 0 -t board -y 2015 > nohup.7115 &
-nohup python3 batch_index.py -p 7114  -s 0 -t board -y 2014 > nohup.7114 &
-nohup python3 batch_index.py -p 7113  -s 0 -t board -y 2013 > nohup.7113 &
-nohup python3 batch_index.py -p 7112  -s 0 -t board -y 2012 > nohup.7112 &
-nohup python3 batch_index.py -p 7111  -s 0 -t board -y 2011 > nohup.7111 &
+nohup python3 batch_index.py -p 7111  -s 2562 -t board -y 2011 > nohup.7111 &
+nohup python3 batch_index.py -p 7112  -s 6580 -t board -y 2012 > nohup.7112 &
+nohup python3 batch_index.py -p 7113  -s 5411 -t board -y 2013 > nohup.7113 &
+nohup python3 batch_index.py -p 7114  -s 7654 -t board -y 2014 > nohup.7114 &
+nohup python3 batch_index.py -p 7115  -s 6273 -t board -y 2015 > nohup.7115 &
+nohup python3 batch_index.py -p 7116  -s 9369 -t board -y 2016 > nohup.7116 &
+nohup python3 batch_index.py -p 7117  -s 6275 -t board -y 2017 > nohup.7117 &
 
-
-nohup python3 batch_index.py -p 7217  -s 0 -t cm    -y 2017 > nohup.7217 &
-nohup python3 batch_index.py -p 7216  -s 0 -t cm    -y 2016 > nohup.7216 &
-nohup python3 batch_index.py -p 7215  -s 0 -t cm    -y 2015 > nohup.7215 &
-nohup python3 batch_index.py -p 7214  -s 0 -t cm    -y 2014 > nohup.7214 &
-nohup python3 batch_index.py -p 7213  -s 0 -t cm    -y 2013 > nohup.7213 &
-nohup python3 batch_index.py -p 7212  -s 0 -t cm    -y 2012 > nohup.7212 &
-nohup python3 batch_index.py -p 7211  -s 0 -t cm    -y 2011 > nohup.7211 &
+nohup python3 batch_index.py -p 7211  -s 5043 -t cm -y 2011 > nohup.7211 &
+nohup python3 batch_index.py -p 7212  -s 4446 -t cm -y 2012 > nohup.7212 &
+nohup python3 batch_index.py -p 7213  -s 3423 -t cm -y 2013 > nohup.7213 &
+nohup python3 batch_index.py -p 7214  -s 2012 -t cm -y 2014 > nohup.7214 &
+nohup python3 batch_index.py -p 7215  -s 1242 -t cm -y 2015 > nohup.7215 &
+nohup python3 batch_index.py -p 7216  -s 3602 -t cm -y 2016 > nohup.7216 &
+nohup python3 batch_index.py -p 7217  -s 3496 -t cm -y 2017 > nohup.7217 &
