2ad781ea613684fb3788955e394c6866f26353e6
HalfEnif <halfenif@gmail.com>
2017-10-24 20:14:29 +0900

    shell change

--- index1.sh
+++ index1.sh
@@ -1,3 +1,6 @@
 nohup python3 batch_index.py -p 7999  -s 0 -t board -y 2017 > nohup.7999 &
 nohup python3 batch_index.py -p 7998  -s 0 -t cm    -y 2017 > nohup.7998 &
 nohup python3 batch_desc.py  -p 7000                        > nohup.7000 &
+nohup python3 batch_desc.py  -p 7001                        > nohup.7001 &
+nohup python3 batch_desc.py  -p 7002                        > nohup.7002 &
+nohup python3 batch_desc.py  -p 7003                        > nohup.7003 &
